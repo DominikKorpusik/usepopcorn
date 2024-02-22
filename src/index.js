5d171eff2e272547f8b8b6bffabc6ef37fc0a2d4
DominikKorpusik <dominik.korpusik@gmail.com>
2024-02-22 20:34:45 +0100

    ADD StarRating Component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./Components/App";
+// import "./index.css";
+// import App from "./Components/App";
+
+import StarRating from "./Components/StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={10} defaultRating={5} />
+    <StarRating
+      color="red"
+      size={50}
+      messages={["Poor", "Fair", "Good", "Very Good", "Excellent"]}
+    />
   </React.StrictMode>
 );
